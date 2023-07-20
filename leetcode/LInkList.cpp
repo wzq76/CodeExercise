42af5c1cd06931dcf0d9986a990a9e266d00634f
wzq12 <wzq1263@163.com>
2023-07-20 11:02:00 +0800

    20230720

--- leetcode/LInkList.cpp
+++ leetcode/LInkList.cpp
@@ -141,7 +141,16 @@ ListNode *swapPairs(ListNode *head) {
  * @return
  */
 ListNode *deleteDuplicates(ListNode *head) {
-
+    if (head == nullptr) return head;
+    ListNode * p=head;
+    while (p->next){
+        if (p->val == p->next->val) {
+            p->next = p->next->next;
+        }else{
+            p= p->next;
+        }
+    }
+    return head;
 }
 
 /**
@@ -178,7 +187,14 @@ ListNode *partition(ListNode *head, int x) {
  * 141. 环形链表
  */
 bool hasCycle(ListNode *head) {
-
+    ListNode * slow = head;
+    ListNode * fast = head;
+    while (fast!= nullptr && fast->next!= nullptr){
+        fast = fast->next->next;
+        slow = slow->next;
+        if(fast == slow) return true;
+    }
+    return false;
 }
 
 /**
